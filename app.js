ca266551ad832d84d12e0b91fa09aea712e776a1
no-one <794174772@qq.com>
2020-11-10 23:42:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.addEventListener('load', async e => {
     if ('serviceWorker' in navigator) {
         try {
             // 注册serviceWorker到sw.js文件中
-            navigator.serviceWorker.register('/sw.js', {scope: '../'});
+            navigator.serviceWorker.register('https://charsh-kop.github.io/pwa-demo/sw.js');
             console.log('SW registered');
         } catch (error) {
             console.log('SW reg failed');
